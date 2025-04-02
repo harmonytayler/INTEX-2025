962d98d170ecb7444c4cece446cca289575ddcd2
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-02 14:49:23 -0600

    Deploying

--- backend/INTEX.API/INTEX.API/obj/Release/net8.0/INTEX.API.AssemblyInfo.cs
+++ backend/INTEX.API/INTEX.API/obj/Release/net8.0/INTEX.API.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8248f863a45a29c9cc1e8daf579bdabb87d8189")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX.API")]
