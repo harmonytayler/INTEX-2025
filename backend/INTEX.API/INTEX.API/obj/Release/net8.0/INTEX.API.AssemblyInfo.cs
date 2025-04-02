64a4915d759fa0257be4b6c6bdd7178c635065ae
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-02 14:58:45 -0600

    Completed setup!

--- backend/INTEX.API/INTEX.API/obj/Release/net8.0/INTEX.API.AssemblyInfo.cs
+++ backend/INTEX.API/INTEX.API/obj/Release/net8.0/INTEX.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8248f863a45a29c9cc1e8daf579bdabb87d8189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b66d5443c7130e46192c8b6507c3618c33b074")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
