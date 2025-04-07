926d8ee4237b7e473fcf5515ebe8c200cb14be7e
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-07 17:22:09 -0600

    undo Login route stuff

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -44,7 +44,7 @@ function LoginPage() {
     : 'https://intex-2025.azurewebsites.net/login?useSessionCookies=true';
 
     try {
-      const response = await fetch("https://intex-2025.azurewebsites.net/login", {
+      const response = await fetch("https://intex-2025.azurewebsites.net/login?useCookies=true", {
         method: 'POST',
         credentials: 'include', // ✅ Ensures cookies are sent & received
         headers: { 'Content-Type': 'application/json' },
