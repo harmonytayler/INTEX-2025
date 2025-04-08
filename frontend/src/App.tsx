c10794abdf6a2ad90a63ec116f766a57700d029b
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-08 13:16:03 -0600

    Revert "Added improved searching functionality, movies in rows based on genre and also individual pages generated dynamically for each movie."
    This reverts commit 74b3ed9a0d4dc9056b3420b18312e9b74b5166b4.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import './style/movies.css';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
 import Header from './components/Header';
 import AdminPage from './pages/AdminPage';
 
@@ -18,7 +16,6 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
           <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </Router>
