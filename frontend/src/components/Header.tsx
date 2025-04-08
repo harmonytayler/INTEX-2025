29f90a2f26c77a0c8bcd385db592965309ed4a9d
tklipp8 <turnerklippel@gmail.com>
2025-04-08 11:12:36 -0600

    Cleared main branch.

--- frontend/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-function Header() {
-  return (
-    <div className="row bg-secondary text-white">
-      <h1 className="Header">Testing INTEX</h1>
-    </div>
-  );
-}
-export default Header;
--- frontend/src/types/env.ts
+++
@@ -1,5 +0,0 @@
-const isLocal = window.location.hostname === 'localhost';
-
-export const backendUrl = isLocal
-  ? 'http://localhost:5000'
-  : import.meta.env.VITE_BACKEND_URL || 'https://intex-2025.azurewebsites.net';
