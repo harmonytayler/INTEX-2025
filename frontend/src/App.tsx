5e0ff0d86dd37b4c39ea63c30356a2c8fa8b362e
Nick Wirthlin <nick.wirthlin@gmail.com>
2025-04-08 20:50:32 -0600

    Create LandingPage and PrivacyPage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,24 +8,26 @@ import MovieDetailsPage from './pages/MovieDetailsPage';
 import Header from './components/Header';
 import AdminPage from './pages/AdminPage';
 import NewUserForm from './components/userInfo/UserInfoForm';
+import LandingPage from './pages/LandingPage';
+import PrivacyPage from './pages/PrivacyPage';
 
 function App() {
   return (
-    <>
-    <Header />
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/register/userinfo" element={<NewUserForm/>} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
-          <Route path="/admin" element={<AdminPage />} />
+    <Router>
+      {/* Header outside Routes so it appears on every page */}
+      <Header />
 
-        </Routes>
-      </Router>
-    </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/register/userinfo" element={<NewUserForm />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+      </Routes>
+    </Router>
   );
 }
 
