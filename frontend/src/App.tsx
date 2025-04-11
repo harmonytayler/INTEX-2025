df4b324cdc6f138b7f065ebd7806d5e05e3f7306
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-10 23:44:37 -0600

    fix router auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,8 +20,8 @@ import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
+    <Router>
+      <AuthProvider>
         <div className="flex flex-col min-h-screen bg-[#0a1a0a] w-full">
           <Header />
           <main className="flex-grow pt-[150px] w-full">
@@ -97,25 +97,24 @@ function App() {
             </Routes>
           </main>
         </div>
-      </Router>
-      <CookieConsent
-        location="bottom"
-        buttonText="I Accept"
-        declineButtonText="I Decline"
-        enableDeclineButton
-        cookieName="cineNicheConsent"
-        style={{ background: "#2B373B" }}
-        buttonStyle={{ color: "#fff", backgroundColor: "#4CAF50", fontSize: "14px", border: "none", padding: "0.5rem 1rem" }}
-        declineButtonStyle={{ background: "#ccc", color: "#000", fontSize: "14px", padding: "0.5rem 1rem", marginLeft: "1rem" }}
-        expires={150}
-      >
-        This website uses cookies to create the user experience. Without accepting, you cannot effectively use the site.{" "}
-        <Link to="/privacy" className="text-yellow-400 underline">
-          Learn more
-        </Link>
-      </CookieConsent>
-    </AuthProvider>
-    
+        <CookieConsent
+          location="bottom"
+          buttonText="I Accept"
+          declineButtonText="I Decline"
+          enableDeclineButton
+          cookieName="cineNicheConsent"
+          style={{ background: "#2B373B" }}
+          buttonStyle={{ color: "#fff", backgroundColor: "#4CAF50", fontSize: "14px", border: "none", padding: "0.5rem 1rem" }}
+          declineButtonStyle={{ background: "#ccc", color: "#000", fontSize: "14px", padding: "0.5rem 1rem", marginLeft: "1rem" }}
+          expires={150}
+        >
+          This website uses cookies to create the user experience. Without accepting, you cannot effectively use the site.{" "}
+          <Link to="/privacy" className="text-yellow-400 underline">
+            Learn more
+          </Link>
+        </CookieConsent>
+      </AuthProvider>
+    </Router>
   );
 }
 
